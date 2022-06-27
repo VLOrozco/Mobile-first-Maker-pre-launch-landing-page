64a6994a9b902598f59c2b0501a1d42fc77d51a1
Veronica Orozco <93060985+VLOrozco@users.noreply.github.com>
2022-06-27 12:18:29 -0500

    refactor code for input and/or email validation

--- main.js
+++ main.js
@@ -1,40 +1,23 @@
+var mailformat = /^\w+([\.-]?\w+)+@\w+([\.:]?\w+)+(\.[a-zA-Z0-9]{2,3})+$/;
+
 function ValidateEmail(inputText) {
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(inputText.value.match(!mailformat)) {
-    document.getElementById("invalid").innerHTML = "Oops! Please add your email";
+  if(inputText.value.length == 0) {
+    document.getElementById("validation").innerHTML = "Oops! Please add your email";
     document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
-    document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
+    document.getElementById("validation").style.color = "rgba(255, 41, 101, 1)";
     document.form1.email.focus();
     return this;
   } else if(inputText.value.match(mailformat)) {
-    document.getElementById("invalid").innerHTML = "Oops! That doesn't look like a valid email address";
+    document.getElementById("validation").innerHTML = "Valid email address!";
     document.getElementById("email").style.outline = "2px solid rgba(62, 233, 229, 1)";
-    document.getElementById("valid").style.color = "rgba(62, 233, 229, 1)";
+    document.getElementById("validation").style.color = "rgba(62, 233, 229, 1)";
     document.form1.email.focus();
     return this;
   } else {
+    document.getElementById("validation").innerHTML = "Oops! That doesn't look like a valid email address";
     document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
-    document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
+    document.getElementById("validation").style.color = "rgba(255, 41, 101, 1)";
     document.form1.email.focus();
     return this;
   }
 }
-
-// function ValidateEmail(inputText) {
-//   const emptyString = "";
-
-//   if(inputText.value.match(emptyString)) {
-//     document.getElementById("invalid").innerHTML = "Oops! Please add your email";
-//     document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
-//     document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
-//     document.form1.email.focus();
-//     return true;
-//   }
-// }
-// else(inputText.value.match(emptyString)) {
-//   document.getElementById("invalid").innerHTML = "Oops! Please add your email";
-//   document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
-//   document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
-//   document.form1.email.focus();
-//   return this;
-// }
