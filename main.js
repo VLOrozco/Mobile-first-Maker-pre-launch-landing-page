799179914a0cee69d008e783ad8a223d83566838
Veronica Orozco <93060985+VLOrozco@users.noreply.github.com>
2022-06-26 23:32:07 -0500

    Update main.js
    create function to check for email validation
    
    +checking for client input/emptyString

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 function ValidateEmail(inputText) {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-  if(inputText.value.match(mailformat)) {
+  if(inputText.value.match(!mailformat)) {
+    document.getElementById("invalid").innerHTML = "Oops! Please add your email";
+    document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
+    document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
+    document.form1.email.focus();
+    return this;
+  } else if(inputText.value.match(mailformat)) {
+    document.getElementById("invalid").innerHTML = "Oops! That doesn't look like a valid email address";
     document.getElementById("email").style.outline = "2px solid rgba(62, 233, 229, 1)";
     document.getElementById("valid").style.color = "rgba(62, 233, 229, 1)";
     document.form1.email.focus();
-    return true;
+    return this;
   } else {
     document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
     document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
     document.form1.email.focus();
-    return false;
+    return this;
   }
 }
+
+// function ValidateEmail(inputText) {
+//   const emptyString = "";
+
+//   if(inputText.value.match(emptyString)) {
+//     document.getElementById("invalid").innerHTML = "Oops! Please add your email";
+//     document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
+//     document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
+//     document.form1.email.focus();
+//     return true;
+//   }
+// }
+// else(inputText.value.match(emptyString)) {
+//   document.getElementById("invalid").innerHTML = "Oops! Please add your email";
+//   document.getElementById("email").style.outline = "2px solid rgba(255, 41, 101, 1)";
+//   document.getElementById("invalid").style.color = "rgba(255, 41, 101, 1)";
+//   document.form1.email.focus();
+//   return this;
+// }
